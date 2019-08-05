f6fb60aed4bb05c02166e792566451feaab22e53
LeviDing <imdingxuewen@gmail.com>
2019-08-05 13:09:23 +0800

    :tada: initial commit

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+hexo clean
+hexo g
+hexo d
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m ':pencil: update content'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:leviding/leviding.github.io.git source
+
+cd -
